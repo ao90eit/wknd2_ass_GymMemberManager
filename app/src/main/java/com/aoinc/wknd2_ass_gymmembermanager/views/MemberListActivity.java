f31d8cdaf07fe6c2770ce28791d86943cb4df890
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-01-10 01:04:05 -0500

    Add simple overflow menu, good start

--- app/src/main/java/com/aoinc/wknd2_ass_gymmembermanager/views/MemberListActivity.java
+++ app/src/main/java/com/aoinc/wknd2_ass_gymmembermanager/views/MemberListActivity.java
@@ -1,8 +1,13 @@
 package com.aoinc.wknd2_ass_gymmembermanager.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.aoinc.wknd2_ass_gymmembermanager.R;
 
@@ -17,6 +22,26 @@ public class MemberListActivity extends AppCompatActivity implements MemberListV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_list);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.member_list_toolbar);
+        setSupportActionBar(toolbar);
+
         memberListPresenter = new MemberListPresenter(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.member_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_action_add_member:
+                startActivity(new Intent(this, AddMemberActivity.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
